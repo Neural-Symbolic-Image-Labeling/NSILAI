bd42a5cdf8f689cd036b4affbe509ec34013e051
GareArc <garethcxy.chen@mail.utoronto.ca>
2022-06-25 19:58:15 -0400

    update dependencies

--- views/__init__.py
+++ views/__init__.py
@@ -2,7 +2,8 @@ import os
 import importlib
 
 module_path = os.path.dirname(os.path.abspath(__file__))
-views = [f for f in os.listdir(module_path) if f.endswith(".py") and f != "__init__.py"]
+views = [f for f in os.listdir(module_path) if f.endswith(
+    ".py") and f != "__init__.py"]
 __all__ = views
 for view in views:
     importlib.import_module("views.%s" % view[:-3])
