9cea6a8197765eae45ad8323c539fc7a28bbdd28
GareArc <garethcxy.chen@mail.utoronto.ca>
2022-06-26 02:26:15 -0400

    add env detection

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+# configuration
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'nsil pog')
 
 import views
--- views/test.py
+++ views/test.py
@@ -1,6 +1,11 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 
 @app.route('/test', methods=['GET'])
 def test():
     return jsonify("Hello World!")
+
+@app.route('/test/objdetec', methods=['POST'])
+def objdetec():
+    data = request.get_json()
+    return jsonify(data["name"])
