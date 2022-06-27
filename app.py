fb1d7e9f55017dcc5c52edf14c7b8de090ab31a2
GareArc <garethcxy.chen@mail.utoronto.ca>
2022-06-27 01:26:59 -0400

    update dependencies

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+# CORS
+cors = CORS(app, resources={r"/api/*": {"origins": os.getenv("CORS_ORIGINS", "*")}})
 # configuration
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'nsil pog')
 
