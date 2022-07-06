1481880b23d9b43d00726685dd836d97b547cac7
ZhouEEEEEE <zhoueric0603@gmail.com>
2022-07-06 10:46:33 -0700

    add FOIL sry for that

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_pymongo import PyMongo
 import base64
 from views.pretrain import pretrain_label
-from views.foil import FOIL
+from views.FOIL import FOIL
 
 import os
 
